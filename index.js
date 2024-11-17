8e657c13e7232cfa47954e55fe0bdddf5a98f875
Yuvi dew <yuvraj@truelink.ai>
2024-11-17 23:38:23 +0530

    adding one logic which is display day of the week with the date and time

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 
 const start = () => {
     const now = new Date();
-    const date = now.toLocaleDateString(); 
-    const time = now.toLocaleTimeString(); 
-    console.log(`The Date: ${date} and The Time: ${time}`);
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    const date = now.toLocaleDateString(undefined, options);
+    const time = now.toLocaleTimeString();
+    console.log(`The Day and Date: ${date} and The Time: ${time}`);
 }
 
 setInterval(() => start() , 1000)
+
+{/** 
+git remote add origin git@github.com:yuvraj-trueinception/Prectice.git
+git branch -M main
+git push -u origin main
+*/}
