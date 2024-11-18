a04f6bba67faea9ce5f42ae6caf4f806d2ea7506
Yuvi dew <yuvraj@truelink.ai>
2024-11-18 23:59:03 +0530

    creating a next js app and adding logic which is display date tile and day in screen

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [dateTime, setDateTime] = useState<string>("");
+
+  useEffect(() => {
+    const updateDateTime = () => {
+      const now = new Date();
+      const options: Intl.DateTimeFormatOptions = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      };
+      const date = now.toLocaleDateString(undefined, options);
+      const time = now.toLocaleTimeString();
+      setDateTime(`The Day and Date: ${date} and The Time: ${time}`);
+    };
+
+    updateDateTime(); 
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
+  return (
+    <div className="h-[100vh] w-full flex items-center justify-center">
+      <h1 className="text-lg">{dateTime}</h1>
+    </div>
+  );
+}
